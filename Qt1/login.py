ba13ccf5e4b57372d3ebe3faac6b5d7a05e67125
prakash <pkombala@gmail.com>
2021-05-29 12:23:42 +0300

    for test

---
+++ Qt1/login.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtGui import QPixmap
+from qt_material import apply_stylesheet
+
+class WelcomeScreen(QDialog):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi("my.ui",self)
+        #self.login.clicked.connect(self.gotologin)
+        #self.create.clicked.connect(self.gotocreate)
+
+
+
+
+
+
+
+app = QApplication(sys.argv)
+welcome = WelcomeScreen()
+widget = QtWidgets.QStackedWidget()
+apply_stylesheet(app, theme='dark_cyan.xml')
+widget.addWidget(welcome)
+widget.setFixedHeight(800)
+widget.setFixedWidth(1200)
+widget.show()
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
